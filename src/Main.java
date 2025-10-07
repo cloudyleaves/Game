620b0f9253886c7c37f8d94536995ba4a69cd802
LailianaMuniz <lailianam@nycstudents.net>
2025-10-07 11:29:34 -0400

    added new main method

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,18 @@ public class Main
         one.getLevel(3).reachGoal();
         one.getLevel(3).setPoints(500);
         System.out.println(two.getScore());
+        Game three = new Game();
+        one.getLevel(1).reachGoal();
+        one.getLevel(1).setPoints(200);
+        one.getLevel(2).reachGoal();
+        one.getLevel(2).setPoints(100);
+        one.getLevel(3).reachGoal();
+        one.getLevel(3).setPoints(500);
+        System.out.println(three.getScore());
+    }
+    public void play()
+    {
+        System.out.println("Enter points");
+
     }
 }
